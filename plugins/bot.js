19ef633a427c5e13c357b82262c569e619feefda
NALVAR47 <92207401+NALVAR47@users.noreply.github.com>
2024-02-02 12:54:07 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["fluxxwave_(lay_with_me)_-_clovis_reyes_&_the_dive_[edit_audio](256k)"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `Hello ${taguser} Need help?  type ∆menu to see full features @𐏕˹ꭗ 𝑵͢ꭺ͢ꮮ͢ꮴ͢ꮜ͢ꮢ ꭗ˼𐏕 `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
